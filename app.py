d999c948d1e225fe4327f408ee642b33eab08697
RodrigoOjedaCcoyo <TU_EMAIL_DE_GITHUB@ejemplo.com>
2026-04-11 15:22:17 -0500

    Actualizaciones

--- app.py
+++ app.py
@@ -251,6 +251,19 @@ const UTM_TERM = "{utms['utm_term']}";
 const parentWindow = window.parent;
 const parentDoc = parentWindow.document;
 
+// --- META PIXEL INJECTION ---
+!function(f,b,e,v,n,t,s)
+{{if(f.fbq)return;n=f.fbq=function(){{n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)}};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}}(parentWindow, parentDoc,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+parentWindow.fbq('init', '1535123857528514');
+parentWindow.fbq('track', 'PageView');
+
 parentWindow.vcpStartTime = parentWindow.vcpStartTime || Date.now();
 parentWindow.vcpMaxScroll = parentWindow.vcpMaxScroll || 0;
 
@@ -308,6 +321,14 @@ parentDoc.addEventListener('click', function(e) {{
                 }},
                 body: payload
             }}).catch(err => console.error(err));
+            
+            // Meta Ads Conversion Event!
+            if (isWa && typeof parentWindow.fbq === 'function') {{
+                parentWindow.fbq('track', 'Lead', {{
+                    content_name: tourName,
+                    currency: 'USD'
+                }});
+            }}
         }}
     }}
 }});
