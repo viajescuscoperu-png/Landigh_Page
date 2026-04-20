4f1394040abcc08eb9d0a8ada6b97acd77a6a20e
RodrigoOjedaCcoyo <TU_EMAIL_DE_GITHUB@ejemplo.com>
2026-04-20 12:23:29 -0500

    Actualizaciones

--- src/data/tours.ts
+++ src/data/tours.ts
@@ -14,7 +14,7 @@ export const tours: Tour[] = [
     name: "Machu Picchu Express",
     oldPrice: "$300 USD",
     price: "SÓ $240 USD",
-    image: "/src/assets/machu.png",
+    image: "/machu.png",
     message: "Olá! Quero aproveitar a oferta de Machu Picchu Express por $240 USD.",
     urgency: "Alta demanda!"
   },
@@ -23,7 +23,7 @@ export const tours: Tour[] = [
     name: "Humantay Lake VIP",
     oldPrice: "$35",
     price: "SÓ $22 USD",
-    image: "/src/assets/humantay.png",
+    image: "/humantay.png",
     message: "Olá! Quero Humantay Lake por USD 22.",
     urgency: "Últimas 5 vagas"
   },
@@ -32,7 +32,7 @@ export const tours: Tour[] = [
     name: "Rainbow Mountain",
     oldPrice: "$35",
     price: "SÓ $22 USD",
-    image: "/src/assets/rainbow.png",
+    image: "/rainbow.png",
     message: "Olá! Quero Rainbow Mountain por USD 22.",
     urgency: "Limitado"
   },
@@ -41,7 +41,7 @@ export const tours: Tour[] = [
     name: "Pallay Punchu Intenso",
     oldPrice: "$50",
     price: "SÓ $35 USD",
-    image: "/src/assets/pallay.png",
+    image: "/pallay.png",
     message: "Olá! Quero Pallay Punchu por USD 35.",
     urgency: "Lugar exclusivo"
   }
